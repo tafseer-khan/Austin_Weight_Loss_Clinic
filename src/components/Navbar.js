61c39b82bd63e6f66692ea4f12835d26ff7c2656
Tafseer-Khan <tafseer.k.khan@gmail.com>
2021-07-31 01:00:35 -0500

    updated screen size responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,26 @@ function Navbar() {
 
     return (
         <div class="topnav">
-            <a className={window.location.pathname === "/" ? "active" : "notactive"} href="/">Home</a>
-            <a className={window.location.pathname === "/about" ? "active" : "notactive"} href="/about">About Us</a>
-            <a className={window.location.pathname === "/services" ? "active" : "notactive"} href="/services">Services</a>
-            <a className={window.location.pathname === "/appointment" ? "active" : "notactive"} href="/appointment">Book Appointment</a>
-            <a className={window.location.pathname === "/contact" ? "active" : "notactive"} href="/contact">Contact and Locations</a>
+            <a className={window.location.pathname === "/" ? "active" : "notactive"} href="/">
+                <span className="large">Home</span>
+                <span className="small">Home</span>
+                </a>
+            <a className={window.location.pathname === "/about" ? "active" : "notactive"} href="/about">
+                <span className="large">About Us</span>
+                <span className="small">About</span>
+                </a>
+            <a className={window.location.pathname === "/services" ? "active" : "notactive"} href="/services">
+                <span className="large">Services</span>
+                <span className="small">Services</span>
+                </a>
+            <a className={window.location.pathname === "/appointment" ? "active" : "notactive"} href="/appointment">
+                <span className="large">Book Appointment</span>
+                <span className="small">Appointment</span>
+                </a>
+            <a className={window.location.pathname === "/contact" ? "active" : "notactive"} href="/contact">
+                <span className="large">Contact and Locations</span>
+                <span className="small">Contact</span>
+                </a>
         </div>
     )
 }
